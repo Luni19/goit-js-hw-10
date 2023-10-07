520fb9c0aa34ecf76e17a22737939c68bf54f731
Konstantin <aziimov1999123@gmail.com>
2023-10-07 09:06:28 +0300

    last commit

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,29 +1,29 @@
-const url1 = 'https://api.thecatapi.com/v1/breeds';
-const url2 = 'https://api.thecatapi.com/v1/images';
+import axios from 'axios';
 
-//Унікальний ключ доступу до Cat API
-const KEY =
-  'live_HEs7npt4enTv8IppoFAzotzjElNW9aw61wQB5T2Fw18DPSakhIju9elgFzOgYqmc';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const KEY = 'live_HEs7npt4enTv8IppoFAzotzjElNW9aw61wQB5T2Fw18DPSakhIju9elgFzOgYqmc';
 
-//Функція, що фетчить список усіх порід котів
+// Функція, що фетчить список усіх порід котів
 const fetchBreeds = () => {
-  return fetch(`${url1}?api_key=${KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios.get(`${BASE_URL}/breeds?api_key=${KEY}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 };
 
-//Функція, що фетчить опис конкретної породи кота по breedId
+// Функція, що фетчить опис конкретної породи кота по breedId
 const fetchCatByBreed = breedId => {
-  return fetch(`${url2}/${breedId}?api_key=${KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios.get(`${BASE_URL}/images/${breedId}?api_key=${KEY}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 };
 
-//Іменований експорт функцій
+// Іменований експорт функцій
 export { fetchBreeds, fetchCatByBreed };
